d861d00e9f25298f559f8ce442ff8e1d0c6e3e1b
DESKTOP-0C0H0I1\moshe <citysys.ltd@gmail.com>
2022-11-15 14:51:59 +0200

    feat: hello world

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    <main>
+        <?php 
+    echo '<h1>Hello world</h1>'
+    ?>
+    </main>
+</body>
+
+</html>
