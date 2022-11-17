4d99fb1df1933ac29f7d794946fb55c636d87b62
DESKTOP-0C0H0I1\moshe <citysys.ltd@gmail.com>
2022-11-17 13:45:28 +0200

    feat: calculator with if statments

--- index.php
+++ index.php
@@ -7,12 +7,26 @@
 
 <body>
     <main>
+
+        <form action="index.php" method="post">
+            num1: <input type="number" name="num1" />
+            op: <input type="text" name="op" />
+            num2: <input type="number" name="num2" />
+
+            <input type="submit" />
+        </form>
         <?php
-        function getMax($num1, $num2)
-        {
-            return $num1 > $num2 ? $num1 : $num2;
+        $num1 = $_POST['num1'];
+        $num2 = $_POST['num2'];
+        $op = $_POST['op'];
+
+        if ($op == '+') {
+            echo $num1 + $num2;
+        } elseif ($op == '-') {
+            echo $num1 + $num2;
+        } elseif ($op == '*') {
+            echo $num1 * $num2;
         }
-        echo getMax(17, 4);
         ?>
     </main>
 </body>
